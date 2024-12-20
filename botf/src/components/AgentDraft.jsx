2a110335df507e0822a480dc836810f9bfddaa5b
R.Abisheik <137792859+Abi-de-jo@users.noreply.github.com>
2024-12-20 21:46:17 +0530

    Update AgentDraft.jsx

--- botf/src/components/AgentDraft.jsx
+++ botf/src/components/AgentDraft.jsx
@@ -259,7 +259,7 @@ ${selectedDraft.parking === 0 ? "❌ Parking" : ""}
           : ""}  
       
 👤 Contact: [@David_Tibelashvili]
-📞 +995 599 20 67 16 | #{selectedDraft?.email?.split('@')[0] || "user"}
+📞 +995 599 20 67 16 | #${selectedDraft?.email?.split('@')[0] || "user"}
        
 ⭐ [Check all listings](https://t.me/rent_tbilisi_ge/9859) | [Reviews](https://t.me/reviews_rent_tbilisi)
         
